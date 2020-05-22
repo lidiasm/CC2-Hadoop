b31355171680de12e2889ad5973a33d6045ef278
Lidia Sánchez Mérida <lidiasm96@correo.ugr.es>
2020-05-22 22:33:42 +0200

    Prueba servidor

--- script-p4.py
+++ script-p4.py
@@ -6,10 +6,10 @@ conf = SparkConf().setAppName("Practica 4. Lidia Sanchez Merida.")
 sc = SparkContext(conf=conf)
 
 """Create a Spark session to create a new dataframe"""
-ss = SparkSession \
-    .builder \
-    .appName("Practica 4. Lidia Sanchez Merida.") \
-    .getOrCreate()
+#ss = SparkSession \
+#    .builder \
+#    .appName("Practica 4. Lidia Sanchez Merida.") \
+#    .getOrCreate()
 
 def read_data():
     """Read the header file"""
@@ -18,24 +18,24 @@ def read_data():
     columns = [inp for inp in headers if "@inputs" in inp]
     """Get each column as a list element and delete '@inputs' and the first blank space"""
     list_columns = columns[0].replace('@inputs', '').replace(' ','').split(',')
+    list_columns.saveAsTextFile("./list_columns/")
+#    """Read data and set the columns"""
+#    data = ss.read.csv("/user/datasets/ecbdl14/ECBDL14_IR2.data", header=False, inferSchema=True)
+#    for c in range(0, len(data.columns)):
+#        data = data.withColumnRenamed(data.columns[c], list_columns[c])
+#
+#    return (data)
 
-    """Read data and set the columns"""
-    data = ss.read.csv("/user/datasets/ecbdl14/ECBDL14_IR2.data", header=False, inferSchema=True)
-    for c in range(0, len(data.columns)):
-        data = data.withColumnRenamed(data.columns[c], list_columns[c])
-
-    return (data)
-
-def create_new_df(df, columns):
-    """Creates a new dataframe with the specified columns"""
-    new_df = df.select(columns)
-    new_df.write.csv('./filteredC.small.training', header=True, mode="overwrite")
+#def create_new_df(df, columns):
+#    """Creates a new dataframe with the specified columns"""
+#    new_df = df.select(columns)
+#    new_df.write.csv('./filteredC.small.training', header=True, mode="overwrite")
 
 if __name__ == "__main__":
     data = read_data()
-    data.show()
+    #data.show()
     #selected_columns = ["PSSM_r1_2_F", "PSSM_r1_-2_F", "PSSM_r2_1_I",
     #    "PSSM_r1_3_F", "PSSM_r1_-1_S", "PSSM_r2_3_M", "class"]
     #create_new_df(data, selected_columns)
-    sc.stop()
+    #sc.stop()
 
