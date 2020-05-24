78879c85724a0dfdb8a34bf0a7d9b6e30e8af86b
Lidia Sánchez Mérida <lidiasm96@correo.ugr.es>
2020-05-24 17:20:39 +0200

    Prueba server

--- models.py
+++ models.py
@@ -82,6 +82,6 @@ if __name__ == "__main__":
   preproc_df = preprocess_df(my_df, my_cols, label_col)
   preproc_df.printSchema()
   """Get the train (70%) and test (30%) dataset"""
-  #train, test = preproc_df.randomSplit([0.7, 0.3], seed = 2020)
+  train, test = preproc_df.randomSplit([0.7, 0.3], seed = 2020)
   """Binomial Logistic Regression"""
-  #binomial_logistic_regression(train, test, 10)
+  binomial_logistic_regression(train, test, 10)
