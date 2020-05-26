2e4fab510efe75a43c5aefbb04ea40f3a4d1afe0
Lidia Sánchez Mérida <lidiasm96@correo.ugr.es>
2020-05-26 20:33:44 +0200

    Mínimo de instancias por nodo=2

--- models.py
+++ models.py
@@ -152,10 +152,10 @@ def naive_bayes(train, test):
     
     return predictions
 
-def decision_trees(train, test, imp, depth):
+def decision_trees(train, test, imp, depth, min_inst):
     """Decision Tree model"""
     dt = DecisionTreeClassifier(labelCol="label", featuresCol="scaledFeatures", 
-        impurity=imp, maxDepth=depth)
+        impurity=imp, maxDepth=depth, minInstancesPerNode=min_inst)
     dt_model = dt.fit(train)
     predictions = dt_model.transform(test)
     
@@ -184,7 +184,7 @@ if __name__ == "__main__":
     #evaluate_model(preds_nb, 'naive.bayes.multinomial')
     
     """Decision Trees models"""
-    preds_dt_gini = decision_trees(train, test, 'gini', 25)
+    preds_dt_gini = decision_trees(train, test, 'gini', 15, 2)
     evaluate_model(preds_dt_gini, 'decision.trees.gini')
-    preds_dt_entropy = decision_trees(train, test, 'entropy', 25)
+    preds_dt_entropy = decision_trees(train, test, 'entropy', 15, 2)
     evaluate_model(preds_dt_entropy, 'decision.trees.entropy')
